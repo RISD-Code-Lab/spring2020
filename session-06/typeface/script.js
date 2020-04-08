ad9ba76a5fd08342b28e32e79c7e3c3f59dad708
motsuka <marie@occupantfonts.com>
2020-04-08 11:05:40 -0400

    add typeface experiment

---
+++ session-06/typeface/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+	$('input[type=range]').on('input', function(){
+		 inputType = $(this).attr('id');
+		 inputValue = $(this).val();
+
+		 if (inputType == 'brow'){
+		 	$('#face').css('--brow-axis', inputValue);
+		 }else{
+		 	$('#face').css('--mouth-axis', inputValue);
+		 }
+
+	});
+
+});
+	
